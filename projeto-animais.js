ec269d054a63f1d290126035d9702bd82b9bcc75
jackson <leojjackson#outlook.com>
2025-05-20 10:29:26 -0300

    mudanças feitas
    foram feitas mudanças no Scroll da página, e atualização no html

--- projeto-animais.js
+++ projeto-animais.js
@@ -1,3 +1,22 @@
 window.revelar = ScrollReveal({reset:true});
 
-ScrollReveal().reveal()
+ScrollReveal().reveal('.part-1', {
+    duration:2000,
+    distance: '200px'
+})
+ScrollReveal().reveal('.part-2', {
+    duration:2000,
+    distance: '200px'
+})
+ScrollReveal().reveal('.part-3', {
+    duration:2000,
+    distance: '200px'
+})
+ScrollReveal().reveal('.part-4', {
+    duration:2000,
+    distance: '200px'
+})
+ScrollReveal().reveal('.part-5', {
+    duration:2000,
+    distance: '200px'
+})
